c385d5fe27e1328492aa1f5cda2aaa7ea1cc912f
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-07-26 11:50:10 +0300

    CustomerDal sınıfı GenericRepository sınıfından inherit edildi ve yazıldı.

---
+++ CasgemUOW.DataAccessLayer/EntityFramework/EfCustomerDal.cs
@@ -0,0 +1,19 @@
+﻿using CasgemUOW.DataAccessLayer.Concrete;
+using CasgemUOW.DataAccessLayer.Repositories;
+using CasgemUOW.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasgemUOW.DataAccessLayer.EntityFramework
+{
+    public class EfCustomerDal : GenericRepository<Customer>
+    {
+        public EfCustomerDal(Context context) : base(context)
+        {
+
+        }
+    }
+}
