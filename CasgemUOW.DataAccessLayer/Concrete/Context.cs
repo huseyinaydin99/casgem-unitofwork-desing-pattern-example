d91340dc7e19f00f9543364be3caac4257d9830f
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-07-26 11:48:15 +0300

    DataAceess kısmında Repository ve Context yazıldı.

---
+++ CasgemUOW.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,20 @@
+﻿using CasgemUOW.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasgemUOW.DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CustomerProcess> CustomerProcesses { get; set; }
+    }
+}
