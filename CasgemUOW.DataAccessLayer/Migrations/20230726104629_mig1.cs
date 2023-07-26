a0c9e3229500a59a6bb7860bcdc9c21976ff2942
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-07-26 14:17:40 +0300

    Controller kısmında alıcı ve vericiyi tespik ettik. İşlem miktarını alıcının hesabına yatırdık ve göndericinin hesabından eksilttik. Tüm bu iki işlemi listeye ekleyip toplu işlem(batch processing) yaptık.

---
+++ CasgemUOW.DataAccessLayer/Migrations/20230726104629_mig1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CasgemUOW.DataAccessLayer.Migrations
+{
+    public partial class mig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerProcesses",
+                columns: table => new
+                {
+                    CustomerProcessId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SenderId = table.Column<int>(type: "int", nullable: false),
+                    ReceiverId = table.Column<int>(type: "int", nullable: false),
+                    Amout = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerProcesses", x => x.CustomerProcessId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerBalance = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerProcesses");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
---
+++ CasgemUOW.PresentationLayer/Controllers/DefaultController.cs
@@ -0,0 +1,40 @@
+﻿using CasgemUOW.BusinessLayer.Abstract;
+using CasgemUOW.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasgemUOW.PresentationLayer.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly ICustomerProcessService _customerProcessService;
+        private readonly ICustomerService _customerService;
+
+        public DefaultController(ICustomerProcessService customerProcessService, ICustomerService customerService)
+        {
+            _customerProcessService = customerProcessService;
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CustomerProcess customerProcess)
+        {
+            var valueSender = _customerService.GetById(customerProcess.SenderId); //göndericiyi saptadık.
+            var valueReceiver = _customerService.GetById(customerProcess.ReceiverId); //alıcıyı saptadık.
+
+            valueReceiver.CustomerBalance += customerProcess.Amout; //alıcının hesabına işlemde belirtilen miktar kadar ekledik.
+            valueSender.CustomerBalance += customerProcess.Amout; //göndericinin hesabından işlemde belirtilen miktar kadar eksilttik.
+            List<Customer> modified = new List<Customer>() //değişen bakiyeleri listeye ekledikki toplu işlem(batch process) yapabilelim.
+            {
+                valueSender, valueReceiver
+            };
+            _customerService.MultiUpdate(modified);
+            return View();
+        }
+    }
+}
