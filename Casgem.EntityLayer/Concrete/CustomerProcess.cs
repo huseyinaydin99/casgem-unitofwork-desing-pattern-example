9669564630e38b3c85afd431ca010ff4574b8d3e
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-07-26 11:15:48 +0300

    Entityler eklendi.

---
+++ Casgem.EntityLayer/Concrete/CustomerProcess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasgemUOW.EntityLayer.Concrete
+{
+    public class CustomerProcess
+    {
+        public int CustomerProcessId { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+        public decimal Amout { get; set; }
+    }
+}
